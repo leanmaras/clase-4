93918191f5103148f1291d9162b102f9916252cb
Leandro Marasca <marasca.leandro@gmail.com>
2022-03-14 20:06:53 -0300

    carga inicial

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let myPerson = new Persona ('juan', 'perez', 23, 1.8);
+
+console.log(myPerson.saludar());
+console.log(myPerson.saludar2());
+console.log(myPerson.soyAlto());
---
+++ js/persona.js
@@ -0,0 +1,20 @@
+class Persona {
+    constructor (nombre, apellido, edad, altura) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.altura = altura;
+    }
+    
+    saludar () {
+        console.log('hola soy ' + this.nombre + ' ' + this.apellido);
+    }
+
+    saludar2 () {
+        console.log(`hola soy  ${this.nombre}  ${this.apellido}`);
+    }
+
+    soyAlto () {
+        return this.altura > 1.8;
+    }
+}
